bcbcb145e9047270038c6ca0d3b4b1524813543b
Amos Barbato <amosbarbato@gmail.com>
2025-01-29 16:55:19 -0300

    refactor: ensure month-options be portuguese language

--- app/_components/time-select.tsx
+++ app/_components/time-select.tsx
@@ -2,20 +2,21 @@
 
 import { useRouter, useSearchParams } from "next/navigation"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/app/_components/ui/select"
+import { CalendarIcon } from "lucide-react"
 
 const MONTH_OPTIONS = [
-  { value: "01", label: "January" },
-  { value: "02", label: "February" },
-  { value: "03", label: "March" },
-  { value: "04", label: "April" },
-  { value: "05", label: "May" },
-  { value: "06", label: "June" },
-  { value: "07", label: "July" },
-  { value: "08", label: "August" },
-  { value: "09", label: "September" },
-  { value: "10", label: "October" },
-  { value: "11", label: "November" },
-  { value: "12", label: "December" },
+  { value: "01", label: "Janeiro" },
+  { value: "02", label: "Fevereiro" },
+  { value: "03", label: "Março" },
+  { value: "04", label: "Abril" },
+  { value: "05", label: "Maio" },
+  { value: "06", label: "Junho" },
+  { value: "07", label: "Julho" },
+  { value: "08", label: "Agosto" },
+  { value: "09", label: "Setembro" },
+  { value: "10", label: "Outubro" },
+  { value: "11", label: "Novembro" },
+  { value: "12", label: "Dezembro" },
 ]
 
 const TimeSelect = () => {
@@ -32,9 +33,13 @@ const TimeSelect = () => {
       onValueChange={(value) => monthChange(value)}
       defaultValue={month ?? ""}
     >
-      <SelectTrigger className="w-[150px] rounded-full">
-        <SelectValue placeholder="Mês" />
+      <SelectTrigger className="w-48 rounded-full px-5">
+        <div className="flex gap-2 items-center">
+          <CalendarIcon className="h-4 w-4" />
+          <SelectValue placeholder="Mês" />
+        </div>
       </SelectTrigger>
+
       <SelectContent>
         {MONTH_OPTIONS.map((option) => (
           <SelectItem key={option.value} value={option.value}>
